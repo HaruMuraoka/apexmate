8e2bd62879da8f64a8a2f54a3183aa092515f3bf
haru.muraoka <haru.3151@icloud.com>
2021-11-06 13:46:12 +0000

    user registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,8 @@
     <div class="center jumbotron">
         <div class="text-center">
             <h1>Let's play with APEXmate</h1>
+            {{-- ユーザ登録ページへのリンク --}}
+            {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
         </div>
     </div>
 @endsection
